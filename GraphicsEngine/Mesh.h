658cfe90159609ab22cf8f908dbde6bbfb4568cf
sharan s <sharan.s@mu-sigma.com>
2018-03-25 22:48:23 +0530

    Added textures

--- GraphicsEngine/Mesh.h
+++ GraphicsEngine/Mesh.h
@@ -7,10 +7,20 @@
 class Vertex
 {
 public:
-	Vertex(const glm::vec3& pos) { this->pos = pos; };
+	Vertex(const glm::vec3& pos,const glm::vec2& texCoord) 
+	{
+		this->pos = pos; 
+		this->texCoord = texCoord;
+	};
+
+	inline glm::vec3* GetPos() { return &pos; }
+	inline glm::vec2* GetTexCoord() { return &texCoord; }
+	
 protected:
 private:
 	glm::vec3 pos;
+	glm::vec2 texCoord;
+	
 };
 
 
@@ -28,6 +38,7 @@ private:
 	enum
 	{
 		POSITION_VB,
+		TEXCOORD_VB,
 		NUM_BUFFERS
 	};
 
